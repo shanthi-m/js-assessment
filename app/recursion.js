c6a206c17ad00484337c7ecf89d76d8a33fc74b6
shanthi-m <malladishanthi@yahoo.com>
2015-07-28 00:27:19 -0500

    changed algorithm

--- app/recursion.js
+++ app/recursion.js
@@ -16,11 +16,12 @@ define(function() {
     	       for (i = 0, len = files.length; i < len; i++) 
     	       { 
     	         file = files[i]; 
-    	         if (typeof file === 'string') { 
+    	         if (typeof file === 'string') 
+    	         { 
     	           if (!dirName || dirs.indexOf(dirName) > -1) { 
     	             listOfFiles.push(files[i]); 
     	             } 
-    	           } 
+    	          } 
     	          else { 
     	              processDir(files[i]); 
     	          } 
@@ -34,26 +35,47 @@ define(function() {
     },
 
     permute: function(arr) {
-    	var permuatationArr = [];
-    	
-    	var recursivefun = function (arrtemp, len) 
+     	/*Heap's algorithm 
+		First we set a counter i  to 0. 
+    	Now we perform the following steps repeatedly, until i  is bigger than N. 
+    	We use the algorithm to generate the (N − 1)! permutations of the first N − 1 elements, 
+    	adjoining the last element to each of these. 
+    	This generates all of the permutations that end with the last element. 
+    	Then if N is odd, we switch the first element and the last one, 
+    	while if N is even we can switch the i th element and the last one 
+    	(there is no difference between N even and odd in the first iteration). 
+        We add one to the counter i and repeat. 
+        In each iteration, the algorithm will produce all of the permutations that end with the element 
+        that has just been moved to the "last" position. 
+*/
+       	var permuatationArr = [];       	
+    	var recursivefun = function (numarr,len) 
     	{
-	        for (var i = 0; i < len; i++)
-	        {
-	             var temp = arrtemp.slice();
-	             temp.splice(len-1-i, 0, temp.splice(len-1, 1)[0]);
-	             if (len >= arr.length) 
-	             {
-	            	 permuatationArr.push(temp);
-	             } 
-	             else 
-	             { 
-	            	 recursivefun(temp, len + 1); 
-	             }
-	        }
-    	 }
-    	recursivefun(arr, 1);
-    	 
+    		if(len == 1)
+    		{
+    			permuatationArr.push(numarr.slice());
+    		}
+    		else
+    		{
+    			for(var i=0; i<len; i++)
+    			{
+    				recursivefun(numarr,len-1);
+    				if(len%2 === 0)
+    					swapfun(numarr,i,len-1);
+    				else
+    					swapfun(numarr,0,len-1);
+    			}
+    		}	
+    	}
+    	recursivefun(arr, arr.length);
+    	
+    	function swapfun(temparr,idx,len)
+    	{
+    		var temp = temparr[idx];
+    		temparr[idx] = temparr[len];
+    		temparr[len] = temp;
+    	}
+    	
     	return permuatationArr;
     }
   };
